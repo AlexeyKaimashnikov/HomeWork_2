e58e44d5a23f8f0b0ec530eda1c6f1d7d92de9ec
Alexei <alexej484@gmail.com>
2022-09-08 16:28:17 +0600

    Task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿ Console.WriteLine("enter a three-digit number:");
+    while(true)
+    {
+        string input = Console.ReadLine();
+        if(!input.Equals("exit"))
+            Console.WriteLine("{0}->{1}",input, input[1]);
+        else
+            break;
+    }
+
