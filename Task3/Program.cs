89680f1115be7603ed0bb5860d0b1c9146f04ca0
Alexei <alexej484@gmail.com>
2022-09-08 22:28:10 +0600

    Task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine ("Enter the number:");
+        string x = Console.ReadLine ();
+        int a = Convert.ToInt32 (x);
+            
+            if (a>5)
+                {
+                  Console.WriteLine(a+" -> yes");
+                }
+            else
+                 { 
+                  Console.WriteLine(a+" -> no");
+                 }
