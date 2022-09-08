a61ee756fa3a1c5fd1e7d89595f75f83c443c0e8
Alexei <alexej484@gmail.com>
2022-09-08 16:36:21 +0600

    Task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n <= 99)
+Console.WriteLine("Третьей цифры нет");
+else
+Console.WriteLine(Convert.ToString(n)[2]);
